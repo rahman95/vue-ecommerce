e058c4d7df10fccd7b9fb61b0a805aee0779bc83
Rahman Younus <rahman_95@live.co.uk>
2018-04-08 18:21:21 +0100

    seperated form into its own component

--- src/components/admin/Create.vue
+++ src/components/admin/Create.vue
@@ -1,55 +1,27 @@
 <template>
   	<div class='container'>
-   		<form @submit.prevent="saveProduct" class='form-horizontal'>
-			<div class='form-group'>
-				<label for='name'>Name</label>
-        <input type="text" placeholder="Name" v-model="model.name" v-validate="'required'" name="name" :class="{'form-control': true, 'error': errors.has('name') }" />
-        <span class="small text-danger" v-show="errors.has('name')">Name is required</span>
-			</div>
-      <div class='form-group'>
-				<label for='name'>Description</label>
-        <textarea placeholder="Description" rows="10" v-model="model.description" v-validate="'required'" name="description" :class="{'form-control': true, 'error': errors.has('description') }" />
-        <span class="small text-danger" v-show="errors.has('description')">Description is required</span>
-			</div>
-			<div class='form-group'>
-				<label for='name'>Price</label>
-        <input type="text" placeholder="Price" v-model="model.price" v-validate="'required|decimal'" name="price" :class="{'form-control': true, 'error': errors.has('price') }" />
-        <span class="small text-danger" v-show="errors.has('price')">Price is required and must be numeric</span>
-			</div>
-			<div class='form-group'>
-				<label for='name'>Image</label>
-        <input type="text" placeholder="Image" v-model="model.image" v-validate="'required|url'" name="image" :class="{'form-control': true, 'error': errors.has('image') }" />
-        <span class="small text-danger" v-show="errors.has('image')">Image is required and must be a valid URL</span>
-			</div>
-      <div class="form-group new-button">
-        <button class="btn btn-success">
-          <i class="fa fa-pencil"></i>
-          <span v-if="isEditing">Update Product</span>
-          <span v-else>Add Product</span>
-        </button>
-      </div>
-   		</form>
-  </div>
+      <product-form @save-product="addProduct" :model="model">
+      </product-form>
+    </div>
 </template>
 
 <script>
+  import ProductFrom from '@/components/forms/Product.vue'
+
   export default {
     name: 'Create',
     data () {
       return {
-        model: {
-          name: null,
-          description: null,
-          price: null,
-          image: null
-        }
+        model: {},
       }
     },
     methods: {
-    saveProduct() {
-      // prints all the errors
-      console.log(this.errors);
+      addProduct (model) {
+        console.log('model', model)
+      }
+    },
+    components: {
+      'product-form': ProductFrom
     }
-  }
   }
 </script>
