47587e68abcf2c4639e08625bb47007dbb3afeaa
Rahman Younus <rahman_95@live.co.uk>
2018-05-01 23:23:27 +0100

    added dotEnv and mongo code

--- api/app.js
+++ api/app.js
@@ -1,4 +1,5 @@
 /* eslint-disable */
+require('dotenv').config()
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -22,5 +23,20 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/', index);
 app.use('/api/v1', api)
+
+mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_URL}`);
+
+app.all('/*', function(req, res, next) {
+  // CORS headers
+  res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  // Set custom headers for CORS
+  res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
+  if (req.method == 'OPTIONS') {
+    res.status(200).end();
+  } else {
+    next();
+  }
+});
 
 module.exports = app;
